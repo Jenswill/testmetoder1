657aab4fb9b66af6bc846fb54b78dd9e38273359
jens <jens11@live.dk>
2020-09-07 14:45:46 +0200

    første commit

---
+++ src/inputOutputJava.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+
+public class inputOutputJava {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("");
+        int a = input.nextInt();
+        float f = input.nextFloat();
+        String username = input.nextLine();
+        input.close();
+
+        System.out.print();
+        System.out.println();
+
+    }
+}
+
